67c3a32da9d3fd141c229c16195f42f320d7c7b5
navisingh14 <navjotgogna@gmail.com>
2016-09-22 22:08:34 -0400

    testing

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,19 +1,22 @@
 class MembersController < ApplicationController
+
+  def index
+    @member = Member.all
+    end
+
+  def show
+    @member= Member.find(params[:id])
+  end
+
   def new
     @member=Member.new
   end
 
-  def index
-@member = Member.all
-  end
-  def show
-  @member= Member.find(params[:id])
-   end
 
   def create
     @member = Member.new(member_params)    # Not the final implementation!
     if @member.save
-      flash[:success] = "Welcome to the Sample App!"
+     # flash[:success] = "Welcome to the Sample App!"
       redirect_to @member
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ resources :members
 root 'welcome#index'
 get  '/signup',  to: 'members#new'
 post '/signup',  to: 'members#create'
+
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
