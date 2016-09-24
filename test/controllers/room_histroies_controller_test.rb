425bf66a1a3c30f9f4a0892d5f9b282e161a6d0b
Li <lshi7@ncsu.edu>
2016-09-23 22:15:29 -0400

    create room&history model/view/controler

---
+++ test/controllers/room_histroies_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class RoomHistroiesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get room_histroies_new_url
+    assert_response :success
+  end
+
+end
---
+++ test/models/roomhistory_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RoomhistoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
