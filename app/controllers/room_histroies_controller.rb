0a36aec307024104fa08ffa3f05f4a0c823e5948
navisingh14 <navjotgogna@gmail.com>
2016-09-24 11:30:47 -0400

    after conflict

--- app/controllers/room_histroies_controller.rb
+++
@@ -1,4 +0,0 @@
-class RoomHistroiesController < ApplicationController
-  def new
-  end
-end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,49 @@
 class RoomsController < ApplicationController
+  before_action :logged_in_member, only: :index
+  # before_action :admin, only: [:create, :destroy]
   def new
+    @room = Room.new
   end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def index
+    @rooms = Room.all
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:success] = "Room added!"
+      redirect_to @room
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    Room.find(params[:id]).destroy
+    flash[:success] = "Room deleted"
+    redirect_to rooms_url
+  end
+
+    private
+
+    def room_params
+      params.require(:room).permit(:number, :building, :size)
+    end
+
+    # def admin
+    #   redirect_to(root_url) unless current_member.admin?
+    # end
+
+    def logged_in_member
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
