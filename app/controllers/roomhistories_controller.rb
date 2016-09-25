ba3e79083216af64371f3cf3c62ad5bcfc9ece44
Li <lshi7@ncsu.edu>
2016-09-25 19:28:44 -0400

    test schedule version 2

--- app/controllers/roomhistories_controller.rb
+++ app/controllers/roomhistories_controller.rb
@@ -13,4 +13,18 @@ class RoomhistoriesController < ApplicationController
   def historybymember
     @roomhistory = Roomhistory.where(userid: params[:userid])
   end
+
+  def create
+    @roomhistory = Roomhistory.new(roomhistory_params)
+    if @roomhistory.save
+      redirect_to schedule_path
+    else
+      render 'new'
+    end
+  end
+
+      private
+      def roomhistory_params
+        params.require(:roomhistory).permit(:number, :userid, :time_from, :time_to)
+      end
 end
