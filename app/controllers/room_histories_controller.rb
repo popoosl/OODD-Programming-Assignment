30dd54277dbfcc042965de3d2f154b99e9118a17
Li <lshi7@ncsu.edu>
2016-09-24 23:31:57 -0400

    rename controller from room_histories to roomhistories

--- app/controllers/room_histories_controller.rb
+++
@@ -1,9 +0,0 @@
-class RoomHistoriesController < ApplicationController
-  def new
-    @roomhistory = Roomhistory.new
-  end
-
-  def show
-    @roomhistory = Roomhistory.find_all_by(number: room.number)
-  end
-end
