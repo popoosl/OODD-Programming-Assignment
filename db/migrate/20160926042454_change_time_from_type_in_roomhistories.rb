dc1b968d4fc1036e76049921e8f20d6daa6a8266
Li <lshi7@ncsu.edu>
2016-10-03 21:04:20 -0400

    aa

--- db/migrate/20160926042454_change_time_from_type_in_roomhistories.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTimeFromTypeInRoomhistories < ActiveRecord::Migration[5.0]
-    def up
-      change_column :roomhistories, :time_from, :datetime
-    end
-
-    def down
-      change_column :roomhistories, :time_from, :date
-    end
-end
--- db/migrate/20160926044304_change_time_to_type_in_roomhistories.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTimeToTypeInRoomhistories < ActiveRecord::Migration[5.0]
-  def up
-    change_column :roomhistories, :time_to, :datetime
-  end
-
-  def down
-    change_column :roomhistories, :time_to, :date
-  end
-end
